7fbf6fe61cbafc527400c1e6071e5f1e4e99ad6d
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2022-01-06 01:19:43 +0530

    Validate Name And Start Date

--- EmployeePayroll-UI/Java-Script/Employee_Payroll.js
+++ EmployeePayroll-UI/Java-Script/Employee_Payroll.js
@@ -1,8 +1,29 @@
-//Changing values Of Salary Acccording To Slider
-const salary = document.querySelector('#salary');
-const salaryOutput = document.querySelector('.salary-output');
-salaryOutput.textContent = salary.value;
-salary.addEventListener('input', function () {
-    salaryOutput.textContent = salary.value;
+window.addEventListener('DOMContentLoaded',(event) =>{
+    salary();
+    nameValidation();
+})
+
+function nameValidation(){
+    //Name Validation
+    const text = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    text.addEventListener('input' , function(){
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
+    if(nameRegex.test(text.value)){
+        textError.textContent = " ";
+    }else{ 
+        textError.textContent = "Name Is Invalid";
+    }
 });
+}
+
+//Changing values Of Salary Acccording To Slider
+function salary() {
+    const salary = document.querySelector('#salary');
+    const salaryOutput = document.querySelector('.salary-output');
+    salaryOutput.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        salaryOutput.textContent = salary.value;
+    });
+}
 
