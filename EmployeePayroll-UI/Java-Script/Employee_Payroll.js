1e90333f3c683ccee8f24f905ab7d376585f0684
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2022-01-07 00:31:03 +0530

    Validate Name And Start Date

--- EmployeePayroll-UI/Java-Script/Employee_Payroll.js
+++ EmployeePayroll-UI/Java-Script/Employee_Payroll.js
@@ -1,29 +1,39 @@
-window.addEventListener('DOMContentLoaded',(event) =>{
-    salary();
-    nameValidation();
-})
-
-function nameValidation(){
+window.addEventListener('DOMContentLoaded', (event) => {
     //Name Validation
     const text = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
-    text.addEventListener('input' , function(){
-    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
-    if(nameRegex.test(text.value)){
-        textError.textContent = " ";
-    }else{ 
-        textError.textContent = "Name Is Invalid";
-    }
-});
-}
+    text.addEventListener('input', function () {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
+        if (nameRegex.test(text.value)) {
+            textError.textContent = " ";
+        } else {
+            textError.textContent = "Name Is Invalid";
+        }
+    });
 
-//Changing values Of Salary Acccording To Slider
-function salary() {
+    //Changing values Of Salary Acccording To Slider
     const salary = document.querySelector('#salary');
     const salaryOutput = document.querySelector('.salary-output');
     salaryOutput.textContent = salary.value;
     salary.addEventListener('input', function () {
         salaryOutput.textContent = salary.value;
     });
-}
+
+    //Start Date Validation
+    const date = document.querySelector("#startDate");
+    date.addEventListener('input', function () {
+        const startDate = new Date(Date.parse(getInputValueById("#day") + " " +
+            getInputValueById("#month") + " " +
+            getInputValueById("#year")));
+        try {
+            (new Employee_PayrollData()).startDate = startDate;
+            setTextValue('.date-error', "");
+        } catch (e) {
+            throw "Date Is Invalid";
+            setTextValue('.date-error', e);
+        }
+    });
+});
+
+
 
--- EmployeePayroll-UI/Java-Script/Employee_PayrollData.js
+++ EmployeePayroll-UI/Java-Script/Employee_PayrollData.js
@@ -38,7 +38,16 @@ class Employee_PayrollData{
         return this.startDate;
     }
     set startDate(startDate){
-        this.startDate = startDate;
+        let now = new Date();
+        if (startDate > now){
+            throw "Start Date Is A Future Date";
+        } 
+        var diff = Math.abs(now.getTime() - startDate.getTime());
+        if(diff / (1000 * 60 * 60 * 24) > 30){
+            throw "Start Date Is Beyond 30 Days";
+        }else{
+            this.startDate = startDate;
+        }
     }
 
     get notes(){
